90647086f297af74de05eec72a0ef3cecbbaf4de
FirozKabir65 <firoj.kabir007@gmail.com>
2020-05-07 15:29:00 +0200

    Structure create and hard code data show

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Weather from './app_component/weather.component'
 import './App.css';
-import 'bootstrap/dist/css/bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'weather-icons/css/weather-icons.css'
 
 function App() {
 	return (
--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -6,11 +6,31 @@ const Weather = () => {
             <div className="card">
                 <h1>Dhaka</h1>
                 <h5 className="py-4">
-                    
+                    <i class="wi wi-day-sunny display-1"></i>
                 </h5>
+                <h1 className="py-2">
+                    25&deg;
+                </h1>
+
+                {
+                    minmaxTemp(24, 19)
+                }
+
+                <h4 className="py-3">
+                    Rainy
+                </h4>
             </div>
         </div>
     );
 };
+
+function minmaxTemp(min,max){
+    return(
+        <h3>
+            <span className="px-4">{min}&deg;</span>
+            <span className="px-4">{max}&deg;</span>
+        </h3>
+    )
+}
 
 export default Weather;
