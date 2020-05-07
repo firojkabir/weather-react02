e63d4d5eb0a2a2f5fcd3a9738fbbf53947ad7282
FirozKabir65 <firoj.kabir007@gmail.com>
2020-05-07 15:20:21 +0200

    Install bootstrap, weather icons

---
+++ src/app_component/weather.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Weather = () => {
+    return (
+        <div className="container">
+            <div className="card">
+                <h1>Dhaka</h1>
+                <h5 className="py-4">
+                    
+                </h5>
+            </div>
+        </div>
+    );
+};
+
+export default Weather;
