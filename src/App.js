458617c8f9e89718dd6a509f7fa1e031a120d188
FirozKabir65 <firoj.kabir007@gmail.com>
2020-05-08 11:18:02 +0200

    Show weather icon based on condition

--- src/App.js
+++ src/App.js
@@ -24,7 +24,13 @@ class App extends React.Component {
 		this.getWeather();
 
 		this.weatherIcon = {
-			Thunderstorm: "wi-thunderstorm"
+			Thunderstorm: "wi-thunderstorm",
+			Drizzle: "wi-sleet",
+			Rain: "wi-storm-showers",
+			Snow: "wi-snow",
+			Atmosphere: "wi-fog",
+			Clear: "wi-day-sunny",
+			Clouds: "wi-day-fog"
 		}
 	}
 
@@ -32,6 +38,34 @@ class App extends React.Component {
 	// 	let cell = Math.floor(temp-273.15);
 	// 	return cell;
 	// }
+
+	get_WeatherIcon(icons,rangeID) {
+		switch(true) {
+			case rangeID >= 200 && rangeID <= 232:
+				this.setState({icon: this.weatherIcon.Thunderstorm});
+				break;
+			case rangeID >= 300 && rangeID <= 321:
+				this.setState({icon: this.weatherIcon.Drizzle});
+				break;
+			case rangeID >= 500 && rangeID <= 531:
+				this.setState({icon: this.weatherIcon.Rain});
+				break;
+			case rangeID >= 600 && rangeID <= 622:
+				this.setState({icon: this.weatherIcon.Snow});
+				break;
+			case rangeID >= 701 && rangeID <= 781:
+				this.setState({icon: this.weatherIcon.Atmosphere});
+				break;
+			case rangeID === 800:
+				this.setState({icon: this.weatherIcon.Clear});
+				break;
+			case rangeID >= 801 && rangeID <= 804:
+				this.setState({icon: this.weatherIcon.Clouds});
+				break;
+			default:
+				this.setState({icon: this.weatherIcon.Clouds});
+		}
+	}
 
 	getWeather = async () => {
 		const api_call = await fetch(
@@ -48,9 +82,10 @@ class App extends React.Component {
 			celsius: response.main.temp,
 			temp_max: response.main.temp_max,
 			temp_min: response.main.temp_min,
-			description: response.weather[0].description,
-			icon: this.weatherIcon.Thunderstorm,
-		})
+			description: response.weather[0].description
+		});
+
+		this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
 	}
 
 	render() {
